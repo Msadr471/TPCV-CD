732cc551e29304c728808181075b36d54a1e8a34
Msadr471 <msadr471@gmail.com>
2025-08-14 22:26:37 +0330

    64

--- test_ondata.py
+++ test_ondata.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     tool_metric = ConfuseMatrixMeter(n_class=2)
 
     dataset = MyDataset(args.datapath, "test")
-    test_loader = DataLoader(dataset, batch_size=32)
+    test_loader = DataLoader(dataset, batch_size=64)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 

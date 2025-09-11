f6dc6bc592009ffb2ec5fa46e3086bd6a76240e4
Mamad Sadr <msadr471@gmail.com>
2025-09-12 02:26:24 +0330

    new req

--- req.sh
+++ req.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-# Install packages
 pip install -qq thop
 pip install -qq albumentations
 
-# Print working dir
 pwd
 cd /content/
 pwd
 
-# Fix /content/Data if it's a file
 if [ -f /content/Data ]; then
     rm /content/Data
 fi
 
-# Make sure /content/Data is a directory
 mkdir -p /content/Data
 
-# Copy zip file
 cp "/content/drive/MyDrive/GoogleColabDrive/CustomDataset/Datasets/Tehran_Mashhad/Tehran/Dataset.zip" /content/Data/
 
-# Unzip only if not already done
+cd /content/Data
+pwd
+
 if [ ! -d /content/Data/Dataset ]; then
-    unzip -q "/content/Data/Dataset.zip" -d /content/Data/ || {
+    unzip -q "Dataset.zip" -d /content/Data/ || {
         echo "❌ Unzip failed!"
         exit 1
     }
@@ -30,17 +27,13 @@ else
     echo "Already unzipped 💁‍♂️"
 fi
 
-# Check the zip file
-ls -lh "/content/Data/Dataset.zip"
+ls -lh "Dataset.zip"
 
-# Compare checksums
 md5sum "/content/drive/MyDrive/GoogleColabDrive/CustomDataset/Datasets/Tehran_Mashhad/Tehran/Dataset.zip"
 md5sum "/content/Data/Dataset.zip"
 
-# Remove Colab default stuff
 rm -rf /content/sample_data
 
-# Go to your repo folder
 cd "/content/TPCV-CD"
 pwd
 ls
